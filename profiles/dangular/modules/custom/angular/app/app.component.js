dcc93ca1aab0ff68abb487080077fa39898152a3
Phéna Proxima <djphenaproxima@gmail.com>
2016-03-08 16:30:02 -0500

    Prepred to implement the persistence layer.

--- profiles/dangular/modules/custom/angular/app/app.component.js
+++ profiles/dangular/modules/custom/angular/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/http'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,16 +10,20 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, http_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent() {
+                function AppComponent(http) {
+                    this.http = http;
                     this.columns = 4;
                 }
                 AppComponent.prototype.setColumns = function (event) {
@@ -28,12 +32,18 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent.prototype.getClasses = function () {
                     return 'clearing-thumbs small-block-grid-' + this.columns;
                 };
+                AppComponent.prototype.persist = function () {
+                    // Actually save the changes here.
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'drupal-view',
-                        template: document.getElementById('views-view').innerHTML
+                        template: document.getElementById('views-view').innerHTML,
+                        providers: [
+                            http_1.HTTP_PROVIDERS
+                        ]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [http_1.Http])
                 ], AppComponent);
                 return AppComponent;
             }());
--- profiles/dangular/modules/custom/angular/app/app.component.ts
+++ profiles/dangular/modules/custom/angular/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from 'angular2/core';
+import {
+    Component
+} from 'angular2/core';
+
+import {
+    Http,
+    Response,
+    HTTP_PROVIDERS
+} from 'angular2/http';
 
 @Component({
   selector: 'drupal-view',
-  template: document.getElementById('views-view').innerHTML
+  template: document.getElementById('views-view').innerHTML,
+  providers: [
+    HTTP_PROVIDERS
+  ]
 })
 export class AppComponent {
+
+  constructor (private http: Http) {
+    // Now we can use this.http for HTTP transactions!
+  }
 
   columns: number = 4;
 
@@ -14,6 +29,10 @@ export class AppComponent {
 
   getClasses () {
     return 'clearing-thumbs small-block-grid-' + this.columns;
+  }
+
+  persist () {
+    // Actually save the changes here.
   }
 
 }
