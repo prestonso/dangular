db520c3b76c0a55bf33b36c75e7dd4b02be31188
Phéna Proxima <djphenaproxima@gmail.com>
2016-03-10 10:24:37 -0500

    Angular now bootstraps to the body element.

--- profiles/dangular/modules/custom/angular/app/app.component.ts
+++ profiles/dangular/modules/custom/angular/app/app.component.ts
@@ -1,87 +1,30 @@
 import {
-    Component,
-    OnInit
+    Component
 } from 'angular2/core';
 
 import {
-    HTTP_PROVIDERS,
-    Response
+    HTTP_PROVIDERS
 } from 'angular2/http';
 
 import {
     Views
 } from './views.service';
 
+import {
+    DrupalView
+} from './drupal-view.component';
+
 @Component({
-  selector: 'drupal-view',
-  template: document.getElementById('views-view').innerHTML,
-  providers: [
-      Views,
-      HTTP_PROVIDERS
-  ],
+    selector: 'body',
+    template: document.body.innerHTML,
+    providers: [
+        Views,
+        HTTP_PROVIDERS
+    ],
+    directives: [
+        DrupalView
+    ]
 })
-export class AppComponent implements OnInit
+export class AppComponent
 {
-    entity: any;
-
-    classes = '';
-
-    submitted = false;
-
-    private columnClass = /-block-grid-([0-9]+)/;
-
-    constructor (private views: Views)
-    {
-        // Angular will take care of dependency injection here.
-    }
-
-    ngOnInit ()
-    {
-        var self = this;
-
-        this.views.load('dangular_image_grid').then(
-            function (response: Response)
-            {
-                self.entity = response.json();
-                self.classes = self.entity.display['default'].display_options.style.options['class'];
-            }
-        );
-    }
-
-    getColumns ()
-    {
-        var match = this.classes.match(this.columnClass);
-
-        return match ? parseInt(match[1]) : 0;
-    }
-
-    setColumns (event: any)
-    {
-        var columns = event.target.value;
-
-        if (this.columnClass.test(this.classes))
-        {
-            this.classes = this.classes.replace(this.columnClass, '-block-grid-' + columns);
-        }
-        else
-        {
-            this.classes += ' small-block-grid-' + columns;
-        }
-
-        this.entity.display['default'].display_options.style.options['class'] = this.classes;
-    }
-
-    persist ()
-    {
-        var self = this;
-
-        self.submitted = true;
-
-        this.views.save(this.entity).then(
-            function ()
-            {
-                self.submitted = false;
-            }
-        );
-    }
 }
